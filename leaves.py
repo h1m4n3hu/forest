66a00fbed195a512bfb7c1a7b2dc22ffaec110fe
Himanshu Soni <57286608+h1m4n3hu@users.noreply.github.com>
2020-11-24 07:54:15 +0530

    Update leaves.py

--- leaves.py
+++ leaves.py
@@ -9,23 +9,12 @@ class Tree:
         self.head=None
 
 
-    def preorder_traversal(self,start):
-        if start:
-            print(start.value,end=" ")               #current node
-            self.preorder_traversal(start.left)      #left tree
-            self.preorder_traversal(start.right)     #right tree
-
-    def inorder_traversal(self,start):
-        if start:
-            self.inorder_traversal(start.left)       #left tree
-            print(start.value,end=" ")               #current node
-            self.inorder_traversal(start.right)      #right tree
-
-    def postorder_traversal(self,start):
-        if start:
-            self.postorder_traversal(start.left)     #left tree
-            self.postorder_traversal(start.right)    #right tree
-            print(start.value,end=" ")               #current node
+    def leaves(self,head):
+        if head:
+            if not head.left and not head.right:
+                print(head.value)
+            self.leaves(head.left)
+            self.leaves(head.right)
 
 
 t=Tree()
@@ -36,9 +25,7 @@ t.head.left.left=Node(4)
 t.head.left.right=Node(5)
 t.head.right.left=Node(6)
 t.head.right.right=Node(7)
-t.preorder_traversal(t.head)    #1 2 4 5 3 6 7 
-t.inorder_traversal(t.head)     #4 2 5 1 6 3 7 
-t.postorder_traversal(t.head)   #4 5 2 6 7 3 1 
+t.leaves(t.head)
 
     #          1
     #         /  \
